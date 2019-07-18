0e2ebf4068a64edef68137d8cb5a05e1a42d0de6
Xinzhao Li <szxzlee@gmail.com>
2019-07-18 18:18:53 +0800

    Persist data with Core Data

--- TodoToDone/Controller/TodoListViewController.swift
+++ TodoToDone/Controller/TodoListViewController.swift
@@ -7,42 +7,54 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var todoArray = [TodoItem]()
     var todoListTableView: UITableView!
+    var searchBarView: UISearchBar!
+    
     let defaults = UserDefaults.standard
     
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     override func loadView() {
         super.loadView()
         
+        // init search bar
+        searchBarView = UISearchBar(frame: .zero)
+        searchBarView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(searchBarView)
+        searchBarView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        searchBarView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        searchBarView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
+        
+        // init table view
         todoListTableView = UITableView(frame: .zero)
         todoListTableView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(todoListTableView!)
         
-        todoListTableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        todoListTableView.topAnchor.constraint(equalTo: self.searchBarView.bottomAnchor).isActive = true
         todoListTableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         todoListTableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         todoListTableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+       
 
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         todoListTableView.delegate = self
         todoListTableView.dataSource = self
         todoListTableView.register(UINib(nibName: "TodoCell", bundle: nil), forCellReuseIdentifier: "customTodoCell")
+        
+        searchBarView.delegate = self
 
         loadTodoItem()
-        
-//        if let itemArray = defaults.array(forKey: "TodoListArray") as? [TodoItem] {
-//            todoArray = itemArray
-//            todoListTableView.reloadData()
-//        }
     }
     
     // MARK: - TableView Data Source Methods
@@ -66,57 +78,79 @@ class TodoListViewController: UIViewController, UITableViewDelegate, UITableView
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         todoArray[indexPath.row].checked = !todoArray[indexPath.row].checked
-        tableView.reloadData()
+        saveTodoItem()
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
     // MARK: - Add new items
     
     @IBAction func onAddButtonPressed(_ sender: UIBarButtonItem) {
-        var newItemName = ""
+        var textField = UITextField()
         let alert = UIAlertController(title: "Add New Todo", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add new item", style: .default) { action in
             // action when user press the add button
-            newItemName = alert.textFields![0].text!
-            let newItem = TodoItem(name: newItemName, done: false)
+            let newItem = TodoItem(context: self.context)
+            newItem.title = textField.text!
+            newItem.checked = false
             self.todoArray.append(newItem)
-            
-            //self.defaults.set(self.todoArray, forKey: "TodoListArray")
             self.saveTodoItem()
             
         }
-        
         alert.addTextField { alertTextField in
             alertTextField.placeholder = "Create new item"
+            textField = alertTextField
         }
-        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        
     }
     
     
     // MARK: - Model manipulation methods
     func saveTodoItem() {
-        let encoder = PropertyListEncoder()
         do {
-            let data = try encoder.encode(self.todoArray)
-            try data.write(to: self.dataFilePath!)
+            try context.save()
         } catch {
-            print(error)
+            print("Error saving context: \(error)")
         }
         self.todoListTableView.reloadData()
     }
     
-    func loadTodoItem() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                todoArray = try decoder.decode([TodoItem].self, from: data)
-            } catch {
-                print(error)
+    func loadTodoItem(with request: NSFetchRequest<TodoItem> = TodoItem.fetchRequest()) {
+        do {
+            todoArray = try context.fetch(request)
+        } catch  {
+            print("Error fetching from context: \(error)")
+        }
+        self.todoListTableView.reloadData()
+    }
+
+}
+
+// MARK: - Search Bar Methods
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<TodoItem> = TodoItem.fetchRequest()
+        
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.predicate = predicate
+        
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+        
+        loadTodoItem(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.count == 0 {
+            loadTodoItem()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
             }
         }
     }
-
 }
 
